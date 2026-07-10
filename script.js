1a1a8070458d3d332ff38414959306bbb6895d07
Paramveer Singh <paramveersinghrayat03@gmail.com>
2026-07-10 21:45:53 +0530

    Add initial HTML structure and JavaScript class for GitHub profile search functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+class GetGithubProfileFinder {
+    constructor() {
+        this.searchBtn = document.getElementById("searchBtn");
+        this.searchInput = document.getElementById("searchInput");
+        this.Skeleton = document.getElementById("Skeleton");
+    }
+
+
+}
